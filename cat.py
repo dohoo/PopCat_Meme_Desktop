b8dac07799336e118f691138606befc21387d876
LIGWA <48171048+dohoo@users.noreply.github.com>
2021-01-22 00:44:01 +0900

    anything is bad

--- cat.py
+++ cat.py
@@ -1,8 +1,12 @@
 import pygame
 
+#-*- coding:utf-8 -*-
+
+#pyinstaller --onefile --icon=icon.ico -w --hidden-import=pygame cat.py
+
 #---- reset ----#
 pygame.init()
-pygame.display.set_caption("POP CAT")
+pygame.display.set_caption("PopCat")
 
 #---- screen ----#
 screen_size_1 = 512
@@ -11,11 +15,11 @@ screen_size = screen_size_1, screen_size_2
 screen = pygame.display.set_mode(screen_size)
 pygame.mouse.set_visible(False)
 
-# #---- image ----#
-image1 = pygame.image.load("1.png")
-image2 = pygame.image.load("2.png")
-image3 = pygame.image.load("3.png")
-image4 = pygame.image.load("4.png")
+#---- image ----#
+image1 = pygame.image.load("C:\\Users\\LEE\\Desktop\\PopCat_Meme_Desktop\\1.png")
+image2 = pygame.image.load("C:\\Users\\LEE\\Desktop\\PopCat_Meme_Desktop\\2.png")
+image3 = pygame.image.load("C:\\Users\\LEE\\Desktop\\PopCat_Meme_Desktop\\3.png")
+image4 = pygame.image.load("C:\\Users\\LEE\\Desktop\\PopCat_Meme_Desktop\\4.png")
 pygame.display.set_icon(image1)
 
 #---- FPS ----#
@@ -32,19 +36,19 @@ while not crashed:
             crashed = True
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 condition += 1
-            elif event.key == pygame.K_z:
+            elif event.key == pygame.K_z or event.key == pygame.K_LEFT:
                 if condition == 1 or condition == 2:
                     condition += 2
-            elif event.key == pygame.K_x:
+            elif event.key == pygame.K_x or event.key == pygame.K_RIGHT:
                 if condition == 3 or condition == 4:
 
                     condition -= 2
             if event.key == pygame.K_ESCAPE:
                 crashed = True
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 condition -= 1
 
     if condition == 1:
