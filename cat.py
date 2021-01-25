0356fa1c8911d6bc4914dfd11120b485f8409a50
dohoo <dohoo2007@naver.com>
2021-01-25 19:56:43 +0900

    Add EasterEgg
    2021_01_25

--- cat.py
+++ cat.py
@@ -22,11 +22,18 @@ image1 = pygame.image.load(os.path.abspath("icon\\1.png"))
 image2 = pygame.image.load(os.path.abspath("icon\\2.png"))
 image3 = pygame.image.load(os.path.abspath("icon\\3.png"))
 image4 = pygame.image.load(os.path.abspath("icon\\4.png"))
+image5 = pygame.image.load(os.path.abspath("icon\\5.png"))
+image6 = pygame.image.load(os.path.abspath("icon\\6.png"))
 pygame.display.set_icon(image1)
 
 #---- sound ----#
 sound1 = pygame.mixer.Sound(os.path.abspath("sound\\1.wav"))
 sound2 = pygame.mixer.Sound(os.path.abspath("sound\\2.wav"))
+
+#---- font ----#
+font = pygame.font.Font(os.path.abspath("font\\SCDream6.otf"), 30)
+img1 = font.render('벌써 10000번 눌렀다.',True,(255, 255, 255))
+img2 = font.render('이제 너 할꺼 하자구',True,(255, 255, 255))
 
 #---- FPS ----#
 FPS = 60
@@ -35,6 +42,8 @@ CLOCK = pygame.time.Clock()
 #---- main loop ----#
 crashed = False
 condition = 1
+ester = 0
+nmb = 1000
 while not crashed:
 
     for event in pygame.event.get():
@@ -43,30 +52,55 @@ while not crashed:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                condition += 1
-                sound2.play()
+                if ester >= nmb:
+                    if condition == 1:
+                        condition += 4
+                    elif condition == 3:
+                        condition += 3
+                else:
+                    condition += 1
+                ester += 1
+                if ester <= nmb*10:
+                    sound2.play()
             elif event.key == pygame.K_z or event.key == pygame.K_LEFT:
                 if condition == 1 or condition == 2:
                     condition += 2
+                if condition == 5:
+                    condition += 1
             elif event.key == pygame.K_x or event.key == pygame.K_RIGHT:
                 if condition == 3 or condition == 4:
-
                     condition -= 2
+                elif condition == 6:
+                    condition -= 1
             if event.key == pygame.K_ESCAPE:
                 crashed = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                condition -= 1
-                sound1.play()
+                if condition == 5:
+                    condition -= 4
+                elif condition == 6:
+                    condition -= 3
+                elif condition == 2 or condition == 4:
+                    condition -= 1
+                if ester <= nmb*10:
+                    sound1.play()
 
-    if condition == 1:
-        screen.blit(image1, (0, 0))
-    elif condition == 2:
-        screen.blit(image2, (0, 0))
-    elif condition == 3:
-        screen.blit(image3, (0, 0))
-    elif condition == 4:
-        screen.blit(image4, (0, 0))
+    if ester >= nmb*10:
+        screen.blit(img1, (50,350))
+        screen.blit(img2, (70,400))
+    else:
+        if condition == 1:
+            screen.blit(image1, (0, 0))
+        elif condition == 2:
+            screen.blit(image2, (0, 0))
+        elif condition == 3:
+            screen.blit(image3, (0, 0))
+        elif condition == 4:
+            screen.blit(image4, (0, 0))
+        elif condition == 5:
+            screen.blit(image5, (0, 0))
+        elif condition == 6:
+            screen.blit(image6, (0, 0))
 
     pygame.display.update()
     CLOCK.tick(FPS)
